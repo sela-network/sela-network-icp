55259f8800e2065b59283b5b6f1a6c70cfb447a9
Ananda Bayu Putra Yudhistira <anandabayu12@gmail.com>
2024-11-14 15:42:36 +0700

    feat: update redirection data

--- src/frontend/src/auth.js
+++ src/frontend/src/auth.js
@@ -2,7 +2,11 @@ import { createActor } from '../../declarations/backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import { fromHexString } from '@dfinity/candid';
-import { Ed25519KeyIdentity, Ed25519PublicKey } from '@dfinity/identity';
+import {
+  Ed25519PublicKey,
+  ECDSAKeyIdentity,
+  DelegationChain,
+} from '@dfinity/identity';
 import { IdbStorage } from '@dfinity/auth-client/lib/cjs/storage';
 import { ws, sendMessage } from './utils/ws';
 
@@ -12,26 +16,18 @@ let actor = backend;
 let authClient; // Declare authClient globally
 let result;
 
-// Helper function to convert ArrayBuffer to hex string
-const bytesToHex = (buffer) => {
-  return Array.from(new Uint8Array(buffer), (byte) =>
-    byte.toString(16).padStart(2, '0')
-  ).join('');
-};
-
 const setupAuth = async () => {
   const url = new URL(window.location.href);
   const sessionKey = url.searchParams.get('sessionkey') ?? ''; // Extract session key from the URL
 
-  let options = { storage: new IdbStorage() };
-
   if (sessionKey) {
     const derPublicKey = fromHexString(sessionKey);
-    const publicKey = Ed25519PublicKey.fromDer(derPublicKey);
-    options.identity = Ed25519KeyIdentity.generate(); // Generate a key identity here
+    const appPublicKey = Ed25519PublicKey.fromDer(derPublicKey);
 
     // Create the Auth instance with options
-    authClient = await AuthClient.create(options); // Initialize authClient here
+    const middleKeyIdentity = await ECDSAKeyIdentity.generate();
+
+    authClient = await AuthClient.create({ identity: middleKeyIdentity }); // Initialize authClient here
 
     console.log('auth: ', authClient);
 
@@ -55,9 +51,6 @@ const setupAuth = async () => {
         agent,
       });
 
-      console.log('actor: ', actor);
-      console.log('identity: ', identity);
-
       // Send a message to WebSocket after successful authentication
       const message = {
         message: `User ${identity.getPrincipal().toString()} has logged in`,
@@ -73,31 +66,25 @@ const setupAuth = async () => {
 
       // Log the principal for debugging
       console.log('Principal:', identity.getPrincipal().toString());
-      let principalID = identity.getPrincipal().toString()
+      let principalID = identity.getPrincipal().toString();
 
       const attributes = []; // Define any attributes if needed
 
       await testDB_operations(principalID);
-    
-      const delegations = identity._delegation.delegations.map(
-        (delegation) => ({
-          delegation: {
-            expiration: delegation.delegation.expiration.toString(),
-            pubkey: bytesToHex(delegation.delegation.pubkey),
-          },
-          signature: bytesToHex(delegation.signature),
-        })
-      );
 
-      const publicKey = bytesToHex(identity.getPublicKey().toDer());
+      // Create new Result
+      if (appPublicKey != null) {
+        let delegationChain = await DelegationChain.create(
+          middleKeyIdentity,
+          appPublicKey,
+          new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
+          { previous: identity.getDelegation() }
+        );
 
-      // Create the final structure
-      result = {
-        delegations,
-        publicKey,
-      };
+        var delegationString = JSON.stringify(delegationChain.toJSON());
 
-      console.log(JSON.stringify(result, null, 2));
+        result = encodeURIComponent(delegationString);
+      }
     } else {
       console.error('Authentication failed');
     }
@@ -117,7 +104,7 @@ const testDB_operations = async (pid) => {
     } else {
       console.error("Failed to store data:", dbInsertResponse.err);
     }
-  
+
     // Retrieve the user data
     const dbGetResponse = await actor.getUserData(pid.toString());
     console.log("DB Get Response:", dbGetResponse);
@@ -174,10 +161,7 @@ const callWhoami = async () => {
     const redirectHost = urlParams.get('host');
 
     if (result) {
-      var delegationString = JSON.stringify(result);
-
-      const encodedDelegation = encodeURIComponent(delegationString);
-      window.location.href = `${redirectScheme}://${redirectHost}?del=${encodedDelegation}&status=true`;
+      window.location.href = `${redirectScheme}://${redirectHost}?del=${result}&status=true`;
     }
   } catch (error) {
     console.error("Error fetching principal ID:", error);
