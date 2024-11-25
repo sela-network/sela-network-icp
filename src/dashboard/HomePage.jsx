ad5f536a1a5a3e8d8e1a861526ece33b3cb1578f
Ananda Bayu Putra Yudhistira <anandabayu12@gmail.com>
2024-11-25 10:46:39 +0700

    refactor: move profile menu as component

--- src/dashboard/HomePage.jsx
+++ src/dashboard/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from './use-auth-client';
 
-import { Nav, Spacing, Header, Text } from './component';
+import { Nav, Spacing, Header, Text, HeaderModal } from './component';
 import Color from './style/Color';
 import { Dashboard, RewardHistory, RewardProgram } from './pages';
 
@@ -72,80 +72,11 @@ const HomePage = () => {
                 principalId={principalId}
                 setProfileMenu={setProfileMenu}
               />
-              <Modal
+              <HeaderModal
                 open={showProfileMenu}
-                onCancel={() => setShowProfileMenu(false)}
-                footer={null}
-                closable={false}
-                style={{
-                  position: 'absolute',
-                  top: 100,
-                  right: 24,
-                  borderRadius: 30,
-                }}
-                width={333}
-                height={382}
-              >
-                <div
-                  style={{
-                    padding: 12,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                  }}
-                >
-                  <Avatar
-                    style={{
-                      backgroundColor: Color.semiGray,
-                    }}
-                    size={48}
-                  >
-                    <Text children="A" size={18} weight="semiBold" />
-                  </Avatar>
-                  <Spacing margin={16} />
-                  <Text
-                    children={'User ID'}
-                    size={18}
-                    color="#000"
-                    weight="semiBold"
-                  />
-                  <Spacing margin={16} />
-                  <Button
-                    shape="round"
-                    size={16}
-                    style={{
-                      backgroundColor: '#000',
-                      borderColor: '#000',
-                      width: 301,
-                      height: 52,
-                    }}
-                  >
-                    <Text
-                      children={'Wallet Connect'}
-                      size={16}
-                      weight="semiBold"
-                    />
-                  </Button>
-                  <Spacing margin={16} />
-                  <Text
-                    children={'Logout'}
-                    size={16}
-                    color="#000"
-                    weight="semiBold"
-                    decoration="underline"
-                    onClick={logout}
-                  />
-                  <Spacing margin={16} />
-                  <Text
-                    children={'Delete Account'}
-                    size={16}
-                    color="#000"
-                    weight="semiBold"
-                    decoration="underline"
-                  />
-                </div>
-              </Modal>
+                onCancel={setShowProfileMenu}
+                logout={logout}
+              />
               <Spacing margin={32} />
               {selectedMenu.children}
             </div>
--- src/dashboard/component/index.jsx
+++ src/dashboard/component/index.jsx
@@ -7,3 +7,4 @@ export { default as Text } from './Text';
 export { default as Container } from './Container';
 export { default as Header } from './Header';
 export { TableConfig, useStyle } from './TableConfig';
+export { default as HeaderModal } from './HeaderModal';
